5230fcc25cb9aba513d5de410a1076c38da73cba
rajur <rajur@172.16.0.13>
2025-11-12 14:58:02 +0530

    Dev added a code extension

--- GitProject01/src/main/java/com/raja/main/B.java
+++ GitProject01/src/main/java/com/raja/main/B.java
@@ -4,5 +4,10 @@ public class B {
 
 	public static void main(String[] args) {
 		System.out.println("Hello B" + " Thanks for adding the file");
+
+		int a = 10;
+		int b = 20;
+		System.out.println("Result is : " + (a+b));
+	
 	}
 }
