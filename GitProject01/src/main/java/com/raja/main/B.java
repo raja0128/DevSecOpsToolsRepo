279dd3168a608cf95838a53a0f7a0b3019cca3fe
rajur <rajur@172.16.0.13>
2025-11-12 14:55:24 +0530

    TL improvised B.java

--- GitProject01/src/main/java/com/raja/main/B.java
+++ GitProject01/src/main/java/com/raja/main/B.java
@@ -3,6 +3,6 @@ package com.raja.main;
 public class B {
 
 	public static void main(String[] args) {
-		System.out.println("Hello B");
+		System.out.println("Hello B" + " Thanks for adding the file");
 	}
 }
